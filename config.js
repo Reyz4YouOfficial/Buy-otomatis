30b6f8a12361c8ed27fad68ecfb27e0396e0fd62
Reyz4You Official <raihanananda767@gmail.com>
2026-02-04 21:53:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
-// config.js
 module.exports = {
     panel: {
-        domain: "https://4youxgodpterodacktyl.kayz.my.id",
+        domain: "https://4youxgodpterodacktyl.kayz.my.id/",
         apikey: "ptla_SR1T1kdy9cTW2aVetwSnMns2wLhaQNLsx7X2XaDR9O7",
         nestId: 5,
         eggId: 15,
@@ -9,15 +8,12 @@ module.exports = {
         startup: "npm start",
         image: "ghcr.io/parkervcp/yolks:nodejs_20"
     },
-
-    // --- KONFIGURASI MIJOO / PAKASIR ---
     pembayaran: {
-        apiKey: "URaa2XAwEHwXrO0LTZcFUfLBozUQqdON", // API Key kamu
-        slug: "reyz4youshop"      // Slug/Plug ID dari dashboard
+        slug: "reyz4youshop", // Ganti dengan slug kamu
+        redirectUrl: "https://buy-panel-otomatis.vercel.app" // Ganti dengan domain vercelmu nanti
     },
-
     hargaPanel: {
         unlimited: 12000,
-        perGB: 1000,  
+        perGB: 1000
     }
 };
