e0e2962074840ea467014ed771745d1ddbc866db
Reyz4You Official <raihanananda767@gmail.com>
2026-02-04 21:26:33 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+// config.js
+module.exports = {
+    panel: {
+        domain: "https://4youxgodpterodacktyl.kayz.my.id",
+        apikey: "ptla_SR1T1kdy9cTW2aVetwSnMns2wLhaQNLsx7X2XaDR9O7",
+        nestId: 5,
+        eggId: 15,
+        locationId: 1,
+        startup: "npm start",
+        image: "ghcr.io/parkervcp/yolks:nodejs_20"
+    },
+
+    // --- KONFIGURASI MIJOO / PAKASIR ---
+    pembayaran: {
+        apiKey: "URaa2XAwEHwXrO0LTZcFUfLBozUQqdON", // API Key kamu
+        slug: "reyz4youshop"      // Slug/Plug ID dari dashboard
+    },
+
+    hargaPanel: {
+        unlimited: 12000,
+        perGB: 1000,  
+    }
+};
